ed87de58e55c53258336ac9606814d4d269eeff6
YAHUI AN <1549472324@qq.com>
2020-07-09 23:33:51 +0800

    update build

---
+++ precache-manifest.487f0707a49927e5121ee34eaeab0080.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62524cc50b553d05990e9e74d2aadedc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3039d491000ca53612ed",
+    "url": "./static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "f9148e618822a7e9d2cd",
+    "url": "./static/css/main.0fb72466.chunk.css"
+  },
+  {
+    "revision": "3039d491000ca53612ed",
+    "url": "./static/js/2.000e5afd.chunk.js"
+  },
+  {
+    "revision": "6e06e1ce2458821fe1aa5784a3672233",
+    "url": "./static/js/2.000e5afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9148e618822a7e9d2cd",
+    "url": "./static/js/main.8e71ef0c.chunk.js"
+  },
+  {
+    "revision": "65b83812d55a405315c1",
+    "url": "./static/js/runtime-main.d5e2f7b5.js"
+  },
+  {
+    "revision": "095f51c3932dcccb53f28ece6341ad4c",
+    "url": "./static/media/index.095f51c3.less"
+  },
+  {
+    "revision": "1192c9caea82d5c3e2abbfc305e8debe",
+    "url": "./static/media/index.1192c9ca.less"
+  },
+  {
+    "revision": "3ab7fa3817efa2c5ab97c1034771d4ff",
+    "url": "./static/media/index.3ab7fa38.less"
+  },
+  {
+    "revision": "589e6bd0b1114622ea3540b0455a3aca",
+    "url": "./static/media/index.589e6bd0.less"
+  },
+  {
+    "revision": "6d835563e66915116b38ba5b4efbf1f3",
+    "url": "./static/media/index.6d835563.less"
+  },
+  {
+    "revision": "793610931ec67927b5b2e2f0ba6b8c58",
+    "url": "./static/media/index.79361093.less"
+  },
+  {
+    "revision": "7c189ea1ee246812079c770a5505de15",
+    "url": "./static/media/index.7c189ea1.less"
+  },
+  {
+    "revision": "83273735ac1f791dce095bc37a0308b3",
+    "url": "./static/media/index.83273735.less"
+  },
+  {
+    "revision": "a14ecc2446f702bc8d6827a5479e0d60",
+    "url": "./static/media/index.a14ecc24.less"
+  },
+  {
+    "revision": "b9685900c9485026249614fa632ccb44",
+    "url": "./static/media/index.b9685900.less"
+  },
+  {
+    "revision": "c17ec543e0d6a592928535f43867f969",
+    "url": "./static/media/index.c17ec543.less"
+  },
+  {
+    "revision": "d4327423db1d1c2abdca329c6c9032dc",
+    "url": "./static/media/index.d4327423.less"
+  }
+]);
